70ae9a353c1aaa720a16efe9e76edd2698e8b38b
KICKSCREW_LSK <reall.lin@kickscrew.com>
2023-03-30 19:13:38 +0800

    mock week 2 active

--- src/sections/MemorabiliaSection/mobile/ScheduleSection/index.js
+++ src/sections/MemorabiliaSection/mobile/ScheduleSection/index.js
@@ -17,11 +17,11 @@ const ScheduleSection = () => {
   useEffect(() => {
     const d = new Date();
 
-    setActiveIndex(
-      d >= Date.parse('17 April 2023 00:00:00 GMT+8') && d <= Date.parse('23 April 2023 23:59:59 GMT+8') ? 1
-        : d >= Date.parse('24 April 2023 00:00:00 GMT+8') && d <= Date.parse('30 April 2023 23:59:59 GMT+8') ? 2
-        : d >= Date.parse('1 May 2023 00:00:00 GMT+8') && d <= Date.parse('7 May 2023 23:59:59 GMT+8') ? 3
-        : d >= Date.parse('8 May 2023 00:00:00 GMT+8') && d <= Date.parse('14 May 2023 23:59:59 GMT+8') ? 4 : Infinity
+    setActiveIndex(2
+      // d >= Date.parse('17 April 2023 00:00:00 GMT+8') && d <= Date.parse('23 April 2023 23:59:59 GMT+8') ? 1
+      //   : d >= Date.parse('24 April 2023 00:00:00 GMT+8') && d <= Date.parse('30 April 2023 23:59:59 GMT+8') ? 2
+      //   : d >= Date.parse('1 May 2023 00:00:00 GMT+8') && d <= Date.parse('7 May 2023 23:59:59 GMT+8') ? 3
+      //   : d >= Date.parse('8 May 2023 00:00:00 GMT+8') && d <= Date.parse('14 May 2023 23:59:59 GMT+8') ? 4 : Infinity
     );
   }, []);
 
