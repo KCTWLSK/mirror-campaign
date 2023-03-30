3d96c07ea42ea19b3d8b95db336695b87bf927ce
KICKSCREW_LSK <reall.lin@kickscrew.com>
2023-03-30 18:03:16 +0800

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,20 @@
+import i18nConfig from "./next-i18next.config.js";
 import publicRuntimeConfig from "./src/configs/index.js";
 
-const isProd = process.env.NODE_ENV === 'production'
+const { i18n } = i18nConfig;
 
 const nextConfig = {
   publicRuntimeConfig,
-  output: 'standalone',
+  i18n,
   images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? '/pages/mirrorcrew' : '',
-  assetPrefix: isProd ? '/pages/mirrorcrew' : undefined,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/zh-HK': { page: '/', query: { __nextDefaultLocale: 'zh-HK', lang: 'zh-HK' } },
-      '/en': { page: '/', query: { __nextDefaultLocale: 'en', lang: 'en' } }
-    }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/socialwall/**',
+      },
+    ],
   },
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { useSelectedLanguage } from "next-export-i18n";
 
 import {
   LandingSection,
@@ -10,30 +9,24 @@ import {
 import { Footer } from "@/components";
 import ScrollProvider from "@/context/scroll";
 
-const Home = () => {
-  const { lang } = useSelectedLanguage();
-
-  return (
-    <>
-      <Head>
-        <title>KICKS CREW X MIRROR LIMITED EDITION MAR 2023</title>
-        <meta name="description" content="KICKS CREW X MIRROR LIMITED EDITION MAR 2023" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ScrollProvider
-        // style={lang === 'en' ? { fontFamily: 'Inter, sans-serif' } : null}
-      >
-        <h1 style={{ display: 'none' }}>KICKS CREW x MIRROR 2023</h1>
-        <LandingSection />
-        <HeroSection />
-        <MemorabiliaSection />
-        <BehindTheScenesSection />
-        <Footer />
-      </ScrollProvider>
-    </>
-  )
-};
+const Home = () => (
+  <>
+    <Head>
+      <title>KICKS CREW X MIRROR LIMITED EDITION MAR 2023</title>
+      <meta name="description" content="KICKS CREW X MIRROR LIMITED EDITION MAR 2023" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <ScrollProvider>
+      <h1 style={{ display: 'none' }}>KICKS CREW x MIRROR 2023</h1>
+      <LandingSection />
+      <HeroSection />
+      <MemorabiliaSection />
+      <BehindTheScenesSection />
+      <Footer />
+    </ScrollProvider>
+  </>
+);
 
 // must have for exporting static html
 Home.getInitialProps = async (ctx) => {
