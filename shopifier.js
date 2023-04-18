18f0cc2a016fd1c670996c1b5d9acc9ca69ac900
KICKSCREW_LSK <reall.lin@kickscrew.com>
2023-04-18 17:10:41 +0800

    feat: add shopifier script to preprocess for deploying onto shopify

---
+++ shopifier.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const fs = require('fs');
+const { html, js, css } = require('js-beautify');
+
+const processFilesInDir = (beautifier, dirPath, processor, shouldProcess = () => true) => {
+  const filenames = fs.readdirSync(dirPath);
+  filenames.forEach((filename) => {
+    if (shouldProcess(filename)) {
+      const filePath = `${dirPath}/${filename}`;
+      const data = fs.readFileSync(
+        filePath,
+        {
+          encoding: 'utf8',
+          flag: 'r',
+        },
+      );
+  
+      fs.writeFileSync(
+        filePath,
+        processor(data, filename)
+        // beautifier(
+        //   processor(data, filename),
+        //   {
+        //     indent_size: 2,
+        //     space_in_empty_paren: true
+        //   },
+        // ),
+      );
+    }
+  });
+};
+
+// *.html
+processFilesInDir(
+  html,
+  path.join(__dirname, 'out'),
+  (data) => {
+    const start = data.indexOf('<div id="__next">');
+    const end = data.indexOf('</body>');
+    data = data.slice(start, end);
+    data = data.replace('"assetPrefix":"/pages/mirrorcrew"', '"assetPrefix":"/pages"');
+    data = data.replaceAll(/\/pages\/mirrorcrew\/_next\/static\/media\/(AK|ALTON|ANSONLO|EDAN|FRANKIE|IAN|JER|JEREMY|KEUNGTO|LOKMAN|STANLEY|TIGER|question_mark)\.[^/.]+\.png/g, "{{ 'mirror-$1.png' | asset_url }}");
+    data = data.replaceAll(/\/pages\/mirrorcrew\/_next\/static\/media\/([^/.]+)\.[^/.]+\.(png|jpg|gif|svg)/g, "{{ '$1.$2' | asset_url }}");
+
+    return data;
+  },
+  (filename) => ['en.html', 'zh-HK.html'].includes(filename),
+);
+
+// *.js
+processFilesInDir(
+  js,
+  path.join(__dirname, 'out/_next/static/chunks'),
+  (data) => {
+    data = data.replaceAll(/\/pages\/mirrorcrew\/_next\/static\/media\/(AK|ALTON|ANSONLO|EDAN|FRANKIE|IAN|JER|JEREMY|KEUNGTO|LOKMAN|STANLEY|TIGER|question_mark)\.[^/.]+\.png/g, "{{ 'mirror-$1.png' | asset_url }}");
+    data = data.replaceAll(/\/pages\/mirrorcrew\/_next\/static\/media\/([^/.]+)\.[^/.]+\.(png|jpg|gif|svg)/g, "{{ '$1.$2' | asset_url }}");
+
+    return data;
+  },
+  (filename) => filename.indexOf('main-') === 0,
+);
+
+processFilesInDir(
+  js,
+  path.join(__dirname, 'out/_next/static/chunks/pages'),
+  (data, filename) => {
+    data = data.replaceAll(/\/pages\/mirrorcrew\/_next\/static\/media\/(AK|ALTON|ANSONLO|EDAN|FRANKIE|IAN|JER|JEREMY|KEUNGTO|LOKMAN|STANLEY|TIGER|question_mark)\.[^/.]+\.png/g, "{{ 'mirror-$1.png' | asset_url }}");
+    data = data.replaceAll(/\/pages\/mirrorcrew\/_next\/static\/media\/([^/.]+)\.[^/.]+\.(png|jpg|gif|svg)/g, "{{ '$1.$2' | asset_url }}");
+
+    if (filename.indexOf('index-') === 0) {
+      console.log(data.match(/\"\/\".concat\(localeValue, \"\#\"\).concat\(constants \/\* SECTION_HERO \*\/ .wK\)/g));
+      console.log(data.match(/\"\/\".concat\(lang, \"\#\"\).concat\(constants \/\* SECTION_HERO \*\/ .wK\)/g));
+      data = data.replaceAll(/\"\/\".concat\(localeValue, \"\#\"\).concat\(constants \/\* SECTION_HERO \*\/ .wK\)/g, "`\/pages\/mirrorcrew\${e === 'en' ? '-en' : ''}#hero`");
+      data = data.replaceAll(/\"\/\".concat\(lang, \"\#\"\).concat\(constants \/\* SECTION_HERO \*\/ .wK\)/g, "`/mirrorcrew${window.location.pathname === '/mirrorcrew-en' ? '-en' : ''}#hero`");
+    }
+
+    return data;
+  },
+  (filename) => filename.indexOf('_app-') === 0 || filename.indexOf('index-') === 0,
+);
+
+const buildId = fs.readdirSync(path.join(__dirname, 'out/_next')).filter((names) => names !== 'static')[0];
+processFilesInDir(
+  js,
+  path.join(__dirname, `out/_next/static/${buildId}`),
+  (data) => {
+    data = data.replace(/static\/css\/[^/.]+\.css/g, "{{ 'mirror-styles-p2.css' | asset_url }}");
+    data = data.replace(/static\/chunks\/([0-9]+)-[^/.]+\.js/g, "{{ 'mirror-$1.js' | asset_url }}");
+    data = data.replace(/static\/chunks\/pages\/index-[^/.]+\.js/g, "{{ 'mirror-index-p2.js' | asset_url }}");
+    data = data.replace(/static\/chunks\/pages\/_error-[^/.]+\.js/g, "{{ 'mirror-_error.js' | asset_url }}");
+
+    return data;
+  },
+  (filename) => filename === '_buildManifest.js',
+);
+
+// *.css
+processFilesInDir(
+  css,
+  path.join(__dirname, 'out/_next/static/css'),
+  (data) => {
+    if (data.indexOf('@font-face') === 0) {
+      data = data.replace("Inter-VariableFont_slnt,wght", "Inter-VariableFont_slnt-wght");
+      data = data.replaceAll(/\/fonts\/([^/.]+).woff2/g, "{{ '$1.woff2' | asset_url }}");
+    }
+
+    return data;
+  },
+);
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,7 +7,7 @@ export const getActiveWeek = () => {
   const d = new Date();
 
   // add random default value for testing purpose
-  return d < Date.parse('24 April 2023 00:00:00 GMT+8') ? 3
+  return d < Date.parse('24 April 2023 00:00:00 GMT+8') ? 1
     : d >= Date.parse('24 April 2023 10:00:00 GMT+8') && d <= Date.parse('30 April 2023 23:59:59 GMT+8') ? 1
     : d >= Date.parse('1 May 2023 10:00:00 GMT+8') && d <= Date.parse('7 May 2023 23:59:59 GMT+8') ? 2
     : d >= Date.parse('8 May 2023 10:00:00 GMT+8') && d <= Date.parse('14 May 2023 23:59:59 GMT+8') ? 3
